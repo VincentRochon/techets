441e49d9071188cf8fe2f422e972248b21ccd613
davidouellet <68755030+davidouellet@users.noreply.github.com>
2020-07-27 14:59:28 -0400

    Update Communication.js

--- Communication.js
+++ Communication.js
@@ -1,21 +1,24 @@
-var Connection = require('tedious').Connection;  
-    var config = {  
-        server: "techets.database.windows.net",  //update me
-        authentication: {
-            type: 'default',
+function Communication() {
+
+    var Connection = require('tedious').Connection;  
+        var config = {  
+            server: "techets.database.windows.net",  //update me
+            authentication: {
+                type: 'default',
+                options: {
+                    userName: 'techets', //update me
+                    password: 'T3chETS123'  //update me
+                }
+            },
             options: {
-                userName: 'techets', //update me
-                password: 'T3chETS123'  //update me
+                // If you are on Microsoft Azure, you need encryption:
+                encrypt: true,
+                database: 'TechETS_DB'  //update me
             }
-        },
-        options: {
-            // If you are on Microsoft Azure, you need encryption:
-            encrypt: true,
-            database: 'TechETS_DB'  //update me
-        }
-    };  
-    var connection = new Connection(config);  
-    connection.on('connect', function(err) {  
-        // If no error, then good to proceed.
-        console.log("Connected");  
-    });
+        };  
+        var connection = new Connection(config);  
+        connection.on('connect', function(err) {  
+            // If no error, then good to proceed.
+            console.log("Connected");  
+        });
+}
